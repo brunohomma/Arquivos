eaac9fffd0e462af7e7755fc9caa95bdfc4edbc0
leonardombonato <leonardo.bonato@usp.br>
2017-05-04 01:17:03 -0300

    Parte 5 e 6 feita

--- leituraArquivoBinario.h
+++ leituraArquivoBinario.h
@@ -1,6 +1,18 @@
 #ifndef _LEITURAARQUIVOBINARIO_H_
 #define _LEITURAARQUIVOBINARIO_H_
 
+struct registro {
+	char *CNPJ; // campo fixo
+	char *nomeSocial; // campo variavel
+	char *nomeFantasia; // campo variavel
+	char *dataRegistro; // campo fixo
+	char *dataCancelamento; // campo fixo 
+	char *motivoCancelamento; // campo variavel
+	char *nomeEmpresa; // campo variavel
+	char *CNPJAuditor; // campo fixo
+};
+
+
 void lerArquivoBinario();
 void parte6(int nroCampo,int nroReg);
 void parte5(int nroReg);
--- trabalho1.c
+++ trabalho1.c
@@ -6,15 +6,16 @@
 
 int main (int argc, char **argv) {
 	FILE *registros = fopen("SCC0215012017projeto01turmaBdadosCompanhias.csv", "r"); // abre o arquivo de entrada para organizar os registros em um arquivos de saidas
-	int op; // auxiliar para escolher as opções do programa
+	int op, nroCampo, nroRegistro; // auxiliar para escolher as opções do programa
 
 	interfaceInicial(); // cria um menu para escolher as operações do programa
 	do {
 		op = fgetc(stdin)-48; // pega um valor do teclado;
 		if (op == 0 || op > 5) printf("Opcão Inválida. Escolha uma opção válida!\n");
 		getchar();
+
 	} while (op == 0 || op > 5);	
-		
+	
 	switch(op) {
 		case 1: // cria o arquivo binario
 			importaRegistro(registros);
@@ -23,9 +24,22 @@ int main (int argc, char **argv) {
 
 		case 2:
 			lerArquivoBinario();
+			break;
+
+		case 3:
+			scanf("%d", &nroRegistro);
+			parte5(nroRegistro);
+			break;
+
+		case 4:
+			scanf("%d", &nroCampo);
+			scanf("%d", &nroRegistro);
+			parte6(nroRegistro, nroCampo);
+			break;
 			
 	}
 
+	
 	fclose(registros);
 	return 0;
 }
